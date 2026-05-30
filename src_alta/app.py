6b6892a29998077d9813ca98928b4fbcd4543584
renan.silva <renan.silva@sankhya.com.br>
2026-05-30 20:11:51 -0300

    Implementa cálculo de preço

--- src_alta/app.py
+++ src_alta/app.py
@@ -3,3 +3,12 @@ def calcular_rentabilidade(custo, venda):
         raise ValueError("Custo deve ser maior que zero")
 
     return ((venda - custo) / custo) * 100
+
+def calcular_preco_venda(custo, markup):
+    if custo <= 0:
+        raise ValueError("Custo deve ser maior que zero")
+
+    if markup < 0:
+        raise ValueError("Markup não pode ser negativo")
+
+    return custo * (1 + markup / 100)
